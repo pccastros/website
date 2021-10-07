749479b4feb3dd9d0418df4813768b7b6fc010ef
Pato <pc.castros@hotmail.com>
2021-10-07 09:22:34 -0500

    Plantilla de Website

---
+++ index.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector("#navbar");
+
+window.addEventListener("scroll", function () {
+  window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
+  window.scrollY < 100 && (navbar.style.background = `transparent`);
+});
