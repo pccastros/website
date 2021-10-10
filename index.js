f3d0bbcd3932db606c8ebf79b7f54d0e0f7aa057
Pato <pc.castros@hotmail.com>
2021-10-10 15:02:22 -0500

    Portfolio

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
+// elements
 const navbar = document.querySelector("#navbar");
-const menu = document.querySelector(".navbar-toggler");
-const links = document.querySelector(".nav-header");
+const menu = document.getElementById('navbarSupportedContent');
+const links = document.querySelectorAll(".nav-link");
+const header = document.querySelector(".nav-header");
 
-window.addEventListener("scroll", function () {
-  window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
-  window.scrollY < 100 && (navbar.style.background = `transparent`);
+// to top when refresh
+window.onbeforeunload = function () {
+  if(window.scrollTo) window.scrollTo(0,0);
+};
+
+// navbar background color
+//window.addEventListener("scroll", function () {
+//  window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
+//  window.scrollY < 100 && (navbar.style.background = `transparent`);
+//});
+
+// close navbar
+function closeNavbar() {
+  menu.classList.remove("show");
+}
+
+// add click function to links
+header.addEventListener("click", closeNavbar);
+links.forEach((link) => {
+  link.addEventListener("click", closeNavbar);
 });
-
-function handleLlinks() {
-  if (window.innerWidth <= 991) {
-    links.classList.toggle("visible");
-    links.classList.toggle("show");
-  }
-}
-
-menu.addEventListener("click", handleLlinks);
-links.addEventListener("click", handleLlinks);
-
-function intersectionHandler(entry) {
-  const id = entry.target.id;
-  const currentlyActive = document.querySelector(".nav-header  .active");
-  const shouldBeActive = document.querySelector(
-    ".nav-header [data-ref=" + id + "]"
-  );
-
-  if (currentlyActive) {
-    currentlyActive.classList.remove("active");
-  }
-  if (shouldBeActive) {
-    shouldBeActive.classList.add("active");
-  }
-}
